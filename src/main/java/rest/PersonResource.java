b7074ee4d2af5188d0eb5889dbf13b552a8705d1
Jon Bertelsen <jobe@cphbusiness.dk>
2019-10-07 17:52:04 +0200

    Minor

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -2,21 +2,46 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import entities.Person;
 import utils.EMF_Creator;
 import facades.PersonFacade;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
+
 @Path("person")
+@OpenAPIDefinition(
+            info = @Info(
+                    title = "CA2 ",
+                    version = "0.4",
+                    description = "Simple API for the CA2",        
+                    contact = @Contact( name = "Jon Bertelsen", email = "jobe@cphbusiness.dk")
+            ),
+          tags = {
+                    @Tag(name = "ca2", description = "API related to CA2")
+              
+            },
+            servers = {
+                    @Server(
+                            description = "For Local host testing",
+                            url = "http://localhost:8080/ca2"
+                    ),
+                    @Server(
+                            description = "Server API",
+                            url = "https://cba.scenius.dk/ca2"
+                    )
+                          
+            }
+    )
+
 public class PersonResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
