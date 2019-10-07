754c0b7a5978fbcee04fe73d1fab8dd1b2b8c608
Jon Bertelsen <jobe@cphbusiness.dk>
2019-10-07 17:05:55 +0200

    Refactored start code to CA2

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 [![Build Status](https://travis-ci.org/jonbertelsen/ca2.svg?branch=master)](https://travis-ci.org/jonbertelsen/ca2)
 
-*This project is meant as start code for projects and exercises given in Flow-1+2 at http://cphbusiness.dk in the Study Program "AP degree in Computer Science"*
+*This project is developed for the CA2 at http://cphbusiness.dk in the Study Program "AP degree in Computer Science"*
 
 *Projects which are expected to use this start-code are projects that require all, or most of the following technologies:*
  - *JPA and REST*
 - *Testing, including database test*
 - *Testing, including tests of REST-API's*
 - *CI and CONTINUOUS DELIVERY*
+- *Swagger and Open API*
 
 ### Preconditions
 *In order to use this code, you should have a local developer setup + a "matching" droplet on Digital Ocean as described in the 3. semester guidelines* 
--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -2,9 +2,9 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import entities.RenameMe;
+import entities.Person;
 import utils.EMF_Creator;
-import facades.FacadeExample;
+import facades.PersonFacade;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -16,8 +16,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
-public class RenameMeResource {
+@Path("person")
+public class PersonResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
                 "pu",
@@ -25,7 +25,7 @@ public class RenameMeResource {
                 "dev",
                 "ax2",
                 EMF_Creator.Strategy.CREATE);
-    private static final FacadeExample FACADE =  FacadeExample.getFacadeExample(EMF);
+    private static final PersonFacade FACADE =  PersonFacade.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
             
     @GET
@@ -36,8 +36,8 @@ public class RenameMeResource {
     @Path("count")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public String getRenameMeCount() {
-        long count = FACADE.getRenameMeCount();
+    public String getPersonCount() {
+        long count = FACADE.getPersonCount();
         //System.out.println("--------------->"+count);
         return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
     }
